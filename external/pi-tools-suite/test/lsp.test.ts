2cf26ab823a8595e4b97e88a436b76ce557c7bcd
vt <vt@anadeainc.com>
2026-06-09 18:07:38 +0200

    Add context handler to LSP registration test

--- external/pi-tools-suite/test/lsp.test.ts
+++ external/pi-tools-suite/test/lsp.test.ts
@@ -370,7 +370,7 @@ describe.serial("LSP library post-edit diagnostics", () => {
 		const { default: registerSuite } = await import("../src/index.js");
 		await registerSuite(pi as any);
 
-		expect([...pi.handlers.keys()].sort()).toEqual(["before_provider_request", "model_select", "session_shutdown", "session_start", "tool_result"]);
+		expect([...pi.handlers.keys()].sort()).toEqual(["before_provider_request", "context", "model_select", "session_shutdown", "session_start", "tool_result"]);
 	});
 
 	test.serial("registers post-edit diagnostics hook without TUI renderers", async () => {
